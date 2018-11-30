e415c06b84e3964ca89675a510cad8c8f85ad64b
Petter Kraabøl <petter.zarlach@gmail.com>
2018-11-30 18:22:19 +0100

    Invalidated twitch oauth and removed script.

--- scripts/chat.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-import twitch
-
-twitch.Chat(channel=f'#{sys.argv[1]}', nickname='zarlach', oauth='oauth:zdi1eezbcumpq5o1ipp0wneks7zn3o').subscribe(
-	lambda message: print(f'{message.sender}: {message.text}\n')
-)
